70741f22e54e6283193f1a41093db417c20bd349
David <dsjgdavid@gmail.com>
2020-04-30 15:23:58 +0200

    Left menu animation completed

--- app/src/main/java/com/example/pickatoast/pickatoast/Fragments/LeftMainMenu.java
+++ app/src/main/java/com/example/pickatoast/pickatoast/Fragments/LeftMainMenu.java
@@ -1,5 +1,6 @@
 package com.example.pickatoast.pickatoast.Fragments;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +8,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
+import com.example.pickatoast.pickatoast.Interfaces.TopMainMenuImpl;
 import com.example.pickatoast.pickatoast.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class LeftMainMenu extends Fragment {
+    //TODO esta mierda esta copiada del menu de arriba, hay que adaptarlo bien
+    //Botones del menu
+    private final int[] MENUBUTTONS={R.id.topMainMenuSideMenuButton,
+            R.id.topMainMenuNotificationButton,
+            R.id.topMainMenuProfileButton};
 
     public LeftMainMenu() {
         // Required empty public constructor
@@ -24,6 +32,30 @@ public class LeftMainMenu extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_left_main_menu, container, false);
+        View menu = inflater.inflate(R.layout.fragment_left_main_menu, container, false);
+
+        ImageButton menuButton;
+
+        //Esto permite identificar automaticamente la actividad en la que nos encontramos al pulsar el boton
+        for(int i =0;i<MENUBUTTONS.length;i++)
+        {
+            menuButton=menu.findViewById(MENUBUTTONS[i]);
+
+            //Numero del boton pulsado segun el array recorrido de todos los botones
+            final int numberOfButtonPressed=i;
+
+            menuButton.setOnClickListener(new View.OnClickListener(){
+
+                @Override
+                public void onClick(View v) {
+                    //Recojo la actividad del boton
+                    Activity currentButtonActivity= getActivity();
+
+                    //Casteo de la actividad al tipo de la interfaz, para poder llamar al metodo
+                    ((TopMainMenuImpl)currentButtonActivity).menu(numberOfButtonPressed);
+                }
+            });
+        }
+        return menu;
     }
 }
--- app/src/main/java/com/example/pickatoast/pickatoast/MainActivity.java
+++ app/src/main/java/com/example/pickatoast/pickatoast/MainActivity.java
@@ -2,17 +2,22 @@ package com.example.pickatoast.pickatoast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import com.example.pickatoast.pickatoast.Interfaces.TopMainMenuImpl;
+import com.example.pickatoast.pickatoast.Services.XmlAnimationService;
 
 
 public class MainActivity extends AppCompatActivity implements TopMainMenuImpl {
+    FrameLayout leftMenu;
 
     Button btn;
     @Override
@@ -20,6 +25,7 @@ public class MainActivity extends AppCompatActivity implements TopMainMenuImpl {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
 
+        leftMenu=findViewById(R.id.leftMenu);
         btn=findViewById(R.id.button);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,5 +39,32 @@ public class MainActivity extends AppCompatActivity implements TopMainMenuImpl {
     @Override
     public void menu(int buttonClicked) {
         Toast.makeText(this,"El boton pulsado es el: "+buttonClicked,Toast.LENGTH_SHORT).show();
+        switch (buttonClicked){
+            case 0:
+                leftMenu.setEnabled(!leftMenu.isEnabled());
+                if(leftMenu.getVisibility()==View.VISIBLE)
+                {
+                    leftMenu.setVisibility(View.INVISIBLE);
+                    leftMenuAnimation2();
+                }
+                else{
+                    leftMenuAnimation();
+                    leftMenu.setVisibility(View.VISIBLE);
+
+                }
+
+                break;
+        }
+    }
+
+    public void leftMenuAnimation(){
+        XmlAnimationService xmlAnimationService = new XmlAnimationService(this);
+        int[] animationArray ={R.anim.right_slice};
+        xmlAnimationService.runAnimationArrayInOrder(leftMenu,animationArray);
+    }
+    public void leftMenuAnimation2(){
+        XmlAnimationService xmlAnimationService = new XmlAnimationService(this);
+        int[] animationArray ={R.anim.left_slice};
+        xmlAnimationService.runAnimationArrayInOrder(leftMenu,animationArray);
     }
 }
