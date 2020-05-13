0241a4158e1737f3a9d943a952cbfc5c4be28ded
crisselene <criisalways@gmail.com>
2020-05-13 14:42:33 +0200

    cambios menu

--- app/src/main/java/com/example/pickatoast/pickatoast/SplashActivity.java
+++ app/src/main/java/com/example/pickatoast/pickatoast/SplashActivity.java
@@ -46,8 +46,8 @@ public class SplashActivity extends AppCompatActivity  {
             public void run() {
 
                 //Cambiar segun a la activity  a a la que quieres que te lleve la animacion
-                Intent guestActivity = new Intent(context, MainActivity.class);
-                startActivity(guestActivity);
+                Intent menu = new Intent(context, MenuTests2.class);
+                startActivity(menu);
                 finish();
 
             }
