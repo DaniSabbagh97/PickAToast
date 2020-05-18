68927637b825abe4c6a0e36ea7be6743969d4485
crisselene <criisalways@gmail.com>
2020-05-18 18:13:15 +0200

    necessary xml

--- app/src/main/java/com/example/pickatoast/pickatoast/SplashActivity.java
+++ app/src/main/java/com/example/pickatoast/pickatoast/SplashActivity.java
@@ -46,8 +46,8 @@ public class SplashActivity extends AppCompatActivity  {
             public void run() {
 
                 //Cambiar segun a la activity  a a la que quieres que te lleve la animacion
-                Intent menu = new Intent(context, MenuTests2.class);
-                startActivity(menu);
+                Intent creat = new Intent(context, CreateEvent.class);
+                startActivity(creat);
                 finish();
 
             }
