db326c20745a752bac617fd49eca119e6d081411
David <dsjgdavid@gmail.com>
2020-05-19 18:48:01 +0200

    Toast pop up removed

--- app/src/main/java/com/example/pickatoast/pickatoast/Fragments/LeftMainMenu.java
+++ app/src/main/java/com/example/pickatoast/pickatoast/Fragments/LeftMainMenu.java
@@ -9,7 +9,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.ImageButton;
 
 import com.example.pickatoast.pickatoast.Interfaces.TopMainMenuImpl;
